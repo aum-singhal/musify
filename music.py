70ae59c75d70672abf368bb9a7a7d2d7789015de
Aum Singhal <pycoder77@gmail.com>
2021-03-11 10:31:49 +0000

    trying

--- music.py
+++ music.py
@@ -1,7 +1,9 @@
 import discord
 from discord.ext import commands
 from random import choice
-import math
+import youtube_dl
+import asyncio
+import urllib.parse, urllib.request, re
 
 client = commands.Bot(command_prefix=commands.when_mentioned_or(">"), description='Relatively simple music bot example')
 
@@ -16,14 +18,108 @@ def colour():
   return choice(l)
 
 
+youtube_dl.utils.bug_reports_message = lambda: ''
+
+ytdl_format_options = {
+  'format': 'bestaudio/best',
+  'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+  'restrictfilenames': True,
+  'noplaylist': True,
+  'nocheckcertificate': True,
+  'ignoreerrors': False,
+  'logtostderr': False,
+  'quiet': True,
+  'no_warnings': True,
+  'default_search': 'auto',
+  'source_address': '0.0.0.0'
+}
+
+ffmpeg_options = {'options': '-vn'}
+
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
+
+class YTDLSource(discord.PCMVolumeTransformer):
+	def __init__(self, source, *, data, volume=0.5):
+		super().__init__(source, volume)
+
+		self.data = data
+
+		self.title = data.get('title')
+		self.url = data.get('url')
+
+	@classmethod
+	async def from_url(cls, url, *, loop=None, stream=False):
+		loop = loop or asyncio.get_event_loop()
+		data = await loop.run_in_executor( None, lambda: ytdl.extract_info(url, download=not stream))
+
+		if 'entries' in data:
+			# take first item from a playlist
+			data = data['entries'][0]
+
+		filename = data['url'] if stream else ytdl.prepare_filename(data)
+		return cls(discord.FFmpegPCMAudio(
+      filename,
+      **ffmpeg_options,
+      before_options= '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5'),
+      data=data
+    )
+
+async def join(ctx):
+    author = ctx.author.voice
+    if not author:
+      em = discord.Embed(
+        title = 'Opps! 😥',
+        description = 'You are not connected to any voice channel. \nTry again after connecting to a voice channel.',
+        colour = discord.Colour.red()
+      )
+      await ctx.send(embed=em)
+    else:
+      author = ctx.author.voice.channel
+      voiceClient = ctx.voice_client
+      if not voiceClient:
+        await author.connect()
+      else:
+        await ctx.send("**Already connected to :**" + author)
+
+
+async def search(ctx, url=""):
+  if 'http://www.youtube.com' in url:
+    return url
+  else:
+    l = url.split(' ')
+    j = ''
+    for i in l:
+      j += i
+      j += '+'
+    url = j
+    htm_content = urllib.request.urlopen(
+      'http://www.youtube.com/results?search_query=' + url
+    )
+    search_results = re.findall(r"watch\?v=(\S{11})", htm_content.read().decode())
+    std = 'http://www.youtube.com/watch?v='
+    url = str(std) + str(search_results[0])
+    return url
+
 class Music(commands.Cog):
   def __init__(self, bot):
     self.bot = bot
 
   @commands.command()
-  async def ping(self, ctx):
-    em = discord.Embed(
-      title = f'Latency: {math.round(client.latency *1000)}ms',
-      colour = colour()
-    )
-    ctx.send(embed=em)
+  async def play(self, ctx, *, url=""):
+    await join(ctx)
+
+    if url == '':
+      embed = discord.Embed(
+        title = 'Opps! 😥',
+        description = "You didn't specified any song to play. \nPlease try again but this time specify a song name or url to play.",
+        colour = discord.Colour.orange()
+      )
+      await ctx.send(embed=embed)
+    else:
+      url = await search(ctx, url)
+
+      async with ctx.typing():
+        player = await YTDLSource.from_url(url, loop= ctx.bot.loop, stream=True, before_options='-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5')
+        ctx.voice_client.play(player, after=lambda e: print('Player error: %s' % e) if e else None)
+      await ctx.send('Now playing: {}'.format(player.title))
