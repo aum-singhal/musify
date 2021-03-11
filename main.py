421e62a1e899fff79ffabbcb7d9dc87a57c98565
Aum Singhal <pycoder77@gmail.com>
2021-03-11 04:23:39 +0000

    jaf

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def colour():
 youtube_dl.utils.bug_reports_message = lambda: ''
 
 ytdl_format_options = {
-  'format': 'bestaudio/best',
+  'format': 'audio/best',
   'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
   'restrictfilenames': True,
   'noplaylist': True,
