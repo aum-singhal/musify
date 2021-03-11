c40c06b0ef79131e6620be08793ca6d6af7814a0
Aum Singhal <pycoder77@gmail.com>
2021-03-11 09:37:00 +0000

    test

--- music.py
+++ music.py
@@ -1,9 +1,19 @@
 import discord
 from discord.ext import commands
-from math import round
-
+from random import choice
 
 client = commands.Bot(command_prefix=commands.when_mentioned_or(">"), description='Relatively simple music bot example')
+
+
+def colour():
+  l = [
+    1752220, 3066993, 3447003, 10181046, 15844367,
+    15105570, 15158332, 3426654, 1146986, 2067276,
+    2123412, 7419530, 12745742, 11027200, 10038562,
+    2899536, 16580705, 12320855
+  ]
+  return choice(l)
+
 
 class Music(commands.Cog):
   def __init__(self, bot):
@@ -12,6 +22,7 @@ class Music(commands.Cog):
   @commands.command()
   async def ping(self, ctx):
     em = discord.Embed(
-      title = f"Pong: {round(client.latency *1000)}ms",
+      title = f'Latency: {round(client.latency *1000)}ms',
+      colour = colour()
     )
     ctx.send(embed=em)
